6e59243a74578e6f95953928ff9a63a4bfeab804
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2022-04-05 15:07:59 -0300

    Add boxplot

--- wq_plots.py
+++ wq_plots.py
@@ -3,6 +3,7 @@ import pandas as pd
 # from pandas_ods_reader import read_ods
 
 import numpy as np
+import matplotlib
 import matplotlib.cbook as cbook
 import matplotlib.image as image
 import matplotlib.pyplot as plt
@@ -16,11 +17,17 @@ import os
 import tempfile
 import config
 
+# Avoid out of memory
+matplotlib.use('agg')
+# Interact
+#matplotlib.use('QtAgg')
+
 
 # PLOT STYLES
 plt.style.use('ggplot')
 #plt.style.use('fivethirtyeight')
 #plt.style.use('seaborn-dark')
+#plt.style.use('seaborn-whitegrid')
 #plt.style.use('bmh')
 
 # Get temporary directory
@@ -78,11 +85,11 @@ for n in range(0,len(rnames)):
     dfs[st]['sflowa7'] = dfs[st]['sflow'].rolling(7).mean()
     # Average 30 days
     dfs[st]['flowa30'] = dfs[st]['flow'].rolling(7).mean()
-    dfs[st]['sflowa30'] = dfs[st]['sflow'].rolling(7).mean()    
+    dfs[st]['sflowa30'] = dfs[st]['sflow'].rolling(7).mean()
 ####################################################
 
 ####################################################
-# Climatic 
+# Climatic
 cnames = ['Las_Brujas']
 for n in range(0,len(cnames)):
     st = cnames[n]
@@ -109,10 +116,10 @@ for ax in axs:
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_flows.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -133,10 +140,10 @@ plot.yaxis.set_label_text('SFlow (L/s/km2)')
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_sflows.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -172,10 +179,10 @@ axs.set_xlim([0,100])
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_pcurves.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -206,6 +213,10 @@ dfwq['CF'] = pd.to_numeric(
     errors='coerce'
 )
 
+dfwq['DBO5'] = pd.to_numeric(dfwq['DBO5 (mg O2/L)'], errors='coerce')
+dfwq['DQO'] = pd.to_numeric(dfwq['Demanda química de oxigeno (DQO) (mg O2/L)'], errors='coerce')
+dfwq['DQO_D_DBO5'] = dfwq['DQO']/dfwq['DBO5']
+
 dfwq['PT'] = pd.to_numeric(dfwq['Fósforo total (µg P/L)'], errors='coerce')/1000
 dfwq['PO4'] = pd.to_numeric(dfwq['Fosfato (ortofosfato) (µg PO4-P/L)'], errors='coerce')/1000
 
@@ -213,8 +224,8 @@ dfwq['NT'] = dfwq['Nitrógeno total (mg N/L)']
 dfwq['NO3'] = pd.to_numeric(dfwq['Nitrato (mg NO3-N/L)'], errors='coerce')
 dfwq['NO2'] = pd.to_numeric(dfwq['Ion Nitrito (mg NO2-N/L)'], errors='coerce')
 dfwq['NH4'] = pd.to_numeric(dfwq['Nitrógeno Amoniacal (amonio) (mg NH4-N/L)'], errors='coerce')
-# Check this calcs
-dfwq['NH4NU'] = dfwq['NH4'] / (1 + 10**-dfwq['PH']/10**-(0.09018+2729.92/(273.15 + dfwq['TEMP']))) 
+# Check this calcs amoníaco no ionizable
+dfwq['NH3NI'] = dfwq['NH4'] / (1 + 10**-dfwq['PH']/10**-(0.09018+2729.92/(273.15 + dfwq['TEMP'])))
 
 dfwq['NT_D_PT'] = dfwq['NT']/dfwq['PT']
 dfwq['NO3_D_PO4'] = dfwq['NO3']/dfwq['PO4']
@@ -251,6 +262,42 @@ dfwqRSJ.set_index('dt', inplace=True)
 
 ###
 # Field
+
+fig, axs = plt.subplots(2, 2, sharex=True, sharey=False)
+fig.set_size_inches(config.FIG_XLSIZE)
+fig.set_dpi(config.FIG_DPI)
+
+boxprops = dict(linestyle='-', linewidth=1, color='black')
+whiskerprops = dict(linestyle='-', linewidth=1, color='black')
+medianprops = dict(linestyle='-', linewidth=2.0, color='black')
+
+# meanpointprops = dict(marker='D', markeredgecolor='black',
+#                       markerfacecolor='firebrick')
+# meanlineprops = dict(linestyle='--', linewidth=1.5, color='blue')
+
+pp = dfwqRSJ.boxplot(
+    column=['TEMP', 'PH', 'CE', 'OD'], by=['Estación'],
+    showmeans=True, meanline=True,
+    boxprops=boxprops,
+    whiskerprops=whiskerprops,
+    medianprops=medianprops,
+    ax=axs, layout=(2,2), grid=True
+)
+# Set logarithmic
+axs[1,0].set_yscale('log')
+# Title
+fig.suptitle('Field data')
+plt.tight_layout()
+plt.savefig(
+     os.path.join(
+        temp_dir,
+        '{}_RSJ_FIELD_BP.pdf'.format(
+            datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        )
+    )
+)
+plt.close(fig)
+
 fig, axs = plt.subplots(5, 1, sharex=True, sharey=False)
 fig.set_size_inches(config.FIG_XXLSIZE)
 fig.set_dpi(config.FIG_DPI)
@@ -277,10 +324,10 @@ axs[4].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSJ_FIELD.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -295,7 +342,7 @@ fig.set_dpi(config.FIG_DPI)
 fflows.plot(x = 'dt', y = 'flow', title='Flow (Picada de Varela, m3/s)', legend=True, grid=True, logy=True, ax=axs[0])
 dfwqRSJ.plot(x = 'dt_y', y = 'flow', title='Flow (Picada de Varela, m3/s)', marker='o', legend=True, grid=True, logy=True, ax=axs[0])
 
-dfwqRSJ.groupby('Estación')['NH4NU'].plot(title='Amoníaco libre (mgN/L)', legend=True, grid=True, logy=True, ax=axs[1])
+dfwqRSJ.groupby('Estación')['NH3NI'].plot(title='Amoníaco libre (mgN/L)', legend=True, grid=True, logy=True, ax=axs[1])
 dfwqRSJ.groupby('Estación')['NO3'].plot(title='Nitratos (mgN/L)', legend=True, grid=True, logy=True, ax=axs[2])
 dfwqRSJ.groupby('Estación')['NO2'].plot(title='Nitritos (mgN/L)', legend=True, grid=True, logy=True, ax=axs[3])
 dfwqRSJ.groupby('Estación')['NT'].plot(title='Nitrógeno total (mgN/L)', legend=True, grid=True, logy=True, ax=axs[4])
@@ -312,10 +359,10 @@ axs[4].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSJ_NITROGEN.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -347,10 +394,10 @@ axs[4].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSJ_NP.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -379,16 +426,14 @@ axs[3].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSJ_NPT.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
 ###
-
-
 
 ###
 # NO3, PO4, NO3/PO4
@@ -408,10 +453,10 @@ axs[3].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSJ_NO3PO4.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -439,11 +484,11 @@ for pn in params:
     plt.tight_layout()
     plt.savefig(
          os.path.join(
-             temp_dir, 
+             temp_dir,
             '{}_RSJ_{}F.pdf'.format(
                 datetime.datetime.now().strftime('%Y%m%d_%H%M%S'),
                 pn
-            )     
+            )
         )
     )
     plt.close(fig)
@@ -503,10 +548,10 @@ axs[4].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSL_FIELD.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -521,7 +566,7 @@ fig.set_dpi(config.FIG_DPI)
 fflows.plot(x = 'dt', y = 'flow', title='Flow (Paso Pache, m3/s)', legend=True, grid=True, logy=True, ax=axs[0])
 dfwqRSL.plot(x = 'dt_y', y = 'flow', title='Flow (Paso Pache, m3/s)', marker='o', legend=True, grid=True, logy=True, ax=axs[0])
 
-dfwqRSL.groupby('Estación')['NH4NU'].plot(title='Amoníaco libre (mgN/L)', legend=True, grid=True, logy=True, ax=axs[1])
+dfwqRSL.groupby('Estación')['NH3NI'].plot(title='Amoníaco libre (mgN/L)', legend=True, grid=True, logy=True, ax=axs[1])
 dfwqRSL.groupby('Estación')['NO3'].plot(title='Nitratos (mgN/L)', legend=True, grid=True, logy=True, ax=axs[2])
 dfwqRSL.groupby('Estación')['NO2'].plot(title='Nitritos (mgN/L)', legend=True, grid=True, logy=True, ax=axs[3])
 dfwqRSL.groupby('Estación')['NT'].plot(title='Nitrógeno total (mgN/L)', legend=True, grid=True, logy=True, ax=axs[4])
@@ -538,10 +583,10 @@ axs[4].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSL_NITROGEN.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -567,10 +612,10 @@ axs[3].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSL_NP.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -598,10 +643,10 @@ axs[3].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSL_NPT.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -624,10 +669,10 @@ axs[3].xaxis.set_minor_locator(mdates.MonthLocator())
 plt.tight_layout()
 plt.savefig(
      os.path.join(
-         temp_dir, 
+         temp_dir,
         '{}_RSL_NO3PO4.pdf'.format(
             datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        )     
+        )
     )
 )
 plt.close(fig)
@@ -654,11 +699,11 @@ for pn in params:
     plt.tight_layout()
     plt.savefig(
          os.path.join(
-             temp_dir, 
+             temp_dir,
             '{}_RSL_{}F.pdf'.format(
                 datetime.datetime.now().strftime('%Y%m%d_%H%M%S'),
                 pn
-            )     
+            )
         )
     )
     plt.close(fig)
